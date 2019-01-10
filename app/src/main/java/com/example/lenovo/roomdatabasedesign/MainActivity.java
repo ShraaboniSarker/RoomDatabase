aad882a0ec88988ed76d931498bcd24c26134219
SHRAABONI <shraabonisarker06@gmail.com>
2019-01-10 12:40:49 +0600

    implemented add functionality

--- app/src/main/java/com/example/lenovo/roomdatabasedesign/MainActivity.java
+++ app/src/main/java/com/example/lenovo/roomdatabasedesign/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.lenovo.roomdatabasedesign;
 
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText userId,userName,userEmail;
+    Button btnAddUser;
+    public static MyAppDatabase appDatabase;
+    public User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userId = findViewById(R.id.user_id);
+        userName =findViewById(R.id.user_name);
+        userEmail = findViewById(R.id.user_email);
+        btnAddUser = findViewById(R.id.add_user);
+
+        appDatabase = Room.databaseBuilder(getApplicationContext(),MyAppDatabase.class,"userdb").allowMainThreadQueries().build();
+
+        btnAddUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = new User();
+                user.setId(userId.getId());
+                user.setName(userName.getText().toString());
+                user.setEmail(userEmail.getText().toString());
+                appDatabase.myDao().addUser(user);
+                Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/lenovo/roomdatabasedesign/MyDao.java
+++ app/src/main/java/com/example/lenovo/roomdatabasedesign/MyDao.java
@@ -1,12 +1,27 @@
 package com.example.lenovo.roomdatabasedesign;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface MyDao  {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void addUser(User user);
+
+    @Query("select * from user")
+    List<User> getUsers();
+
+    @Delete
+    void deleteUser(User user);
+
+    @Update
+    void updateUser(User user);
 
 }
